5719d3549e381557842e84ad5b113c9af976379d
Muahmmed Favas <favasfvs111@gmail.com>
2024-09-19 21:46:08 -0700

    implented JWT for saving user details in client side like session in server

--- src/pages/user-side/LoginPage.jsx
+++ src/pages/user-side/LoginPage.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import {useForm} from "react-hook-form"
 import {Link} from "react-router-dom"
 import bgimg from "../../assets/aa.png"
+import axiosInstance from "../../instatnce/axiosInstance";
+import { useNavigate } from "react-router-dom";
 function UserLogin(){
 
+    const navigate = useNavigate()
     const {register,handleSubmit, formState: {errors},} = useForm();
+    const [errorMessage, setErrorMessage] = useState("");
 
-    const onSubmit = (data)=>{
-        console.log(data)
+    const onSubmit = async (data)=>{
+        try{
+        const response = await axiosInstance.post('/user/login',{
+            email:data.email,
+            password:data.password
+        });
+        // checks teh login is sucuss and get the tocken
+        if(response.status === 200){
+            const {token} = response.data
+
+            // store the token in local storage
+            localStorage.setItem('authToken',token);
+            navigate("/user/userHome")
+        }else{
+            setErrorMessage(response.data.message)
+        }
+    }catch(error){
+        setErrorMessage("Internal server error")
+        console.error("Login error:",error)
+    }
     }
     return(
         <div className="bg-cover h-screen   " style={{backgroundImage: `url(${bgimg})`}}>
@@ -30,6 +53,8 @@ function UserLogin(){
 
             <input type="submit" />
 
+        {/* Display error message if login fails */}
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
             </form>
 
             <Link to={"/UserSign"}><button>Signup</button></Link>
